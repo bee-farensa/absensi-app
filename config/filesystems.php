bf7d35b73ec28a9250ff11f4cf2457295cd75625
bee-farensa <farensanindya04@gmail.com>
2026-06-07 18:17:33 +0700

    add cloudinary di config

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,11 +37,14 @@ return [
             'throw' => false,
             'report' => false,
         ],
+        'cloudinary' => [
+            'driver' => 'cloudinary',
+        ],
 
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
+            'url' => rtrim(env('APP_URL', 'http://localhost'), '/') . '/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
