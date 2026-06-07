ae9e70ffb901f044ded2930f1371a26fab5e7edf
bee-farensa <farensanindya04@gmail.com>
2026-06-07 20:26:42 +0700

    edit variable

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // Force HTTPS di production
-        if (env('APP_ENV') === 'production') {
+        if (app()->environment('production') || config('app.env') === 'production') {
             URL::forceScheme('https');
         }
     }
