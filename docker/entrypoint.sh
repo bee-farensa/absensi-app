e29080dc051dc9492efbb4d5911f81d1884e699a
bee-farensa <farensanindya04@gmail.com>
2026-07-24 09:57:53 +0700

    fix port 80 aws

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# Render inject $PORT secara dinamis, default 10000 kalau lokal
-PORT="${PORT:-10000}"
-sed -i "s/PORT_PLACEHOLDER/$PORT/g" /etc/nginx/nginx.conf
-
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
